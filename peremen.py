7e1a4068257011faa43ab4a9037cb7168f04d8c9
Bush21aaa <BUSH21AAA@YANDEX.RU>
2024-11-11 23:27:19 +0300

    Add files via upload

---
+++ peremen.py
@@ -0,0 +1,5 @@
+namber_home_work = 12
+namber_hours = 1.5
+kurs_name = 'Python'
+time_one_task = namber_hours / namber_home_work
+print('Курс: ' + kurs_name + ', ' + 'всего задач: ' + str(namber_home_work) + ', '  + 'затрачено часов: ' + str(namber_hours) + ', ' + 'среднее время выполнения ' + str(time_one_task) + ' часа.')
